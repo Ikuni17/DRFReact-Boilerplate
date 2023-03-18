74f965e21dbe3b91367f42f4ef097903c4fb7496
Ikuni17 <white.brad17@gmail.com>
2023-03-17 18:31:41 -0600

    Backend and api started

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+# from django.urls import path
+# from api.views import SomeView
+
+# urlpatterns = [
+#     path('someview/', SomeView.as_view()),
+# ]
--- backend/urls.py
+++ backend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter(trailing_slash=False)
 
 urlpatterns = [
+    path('', include(router.urls)),
+    # TODO enable with someview
+    # path('api/', include('api.urls')),
     path("admin/", admin.site.urls),
 ]
